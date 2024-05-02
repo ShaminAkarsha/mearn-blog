c9806f2cce0427b1b594687c8a1e569c6b83859b
shaminakarsha <shaminakarsha@gmail.com>
2024-05-02 17:32:52 +0530

    Create user model

---
+++ api/models/User.model.js
@@ -0,0 +1,22 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+},{timeStamp:true}
+);
+
+const User = mongoose.model('User',userShema);
