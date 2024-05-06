b5e21e98d5d5ce9de0d12a037578c641b8a67abc
shaminakarsha <shaminakarsha@gmail.com>
2024-05-06 14:12:13 +0530

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Dashboard from "./pages/Dashboard"
-import Projects from "./pages/Projects"
-import Header from "./component/Header"
-import Footer from "./component/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import PrivateRoute from "./component/PrivateRoute";
 
 export default function App() {
   return (
@@ -17,10 +18,12 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
