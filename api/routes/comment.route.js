d3193826a217d28b6cc679467af1c5724f3d1f4b
shaminakarsha <shaminakarsha@gmail.com>
2024-05-16 00:29:47 +0530

    add edit functionality to the comment section

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import exprss from "express";
-import { createComment, getPostComment, likeComment } from "../controllers/comment.controller.js";
+import { createComment, getPostComment, likeComment, editComment } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js"
 
 
@@ -8,4 +8,6 @@ const router = exprss.Router();
 router.post("/create", verifyToken , createComment);
 router.get("/getPostComments/:postId", getPostComment);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
+
 export default router;
