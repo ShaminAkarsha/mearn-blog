d8e9963fe2d26eec45c6bdc6e13be5fa9c386629
shaminakarsha <shaminakarsha@gmail.com>
2024-05-16 16:51:25 +0530

    Add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,12 +4,14 @@ import { Link, useParams } from "react-router-dom";
 import GridLoader from "react-spinners/GridLoader";
 import CallToAction from "../component/CallToAction";
 import CommentSection from "../component/CommentSection";
+import PostCard from "../component/PostCard";
 
 export default function PostPage() {
   const { postslug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -35,6 +37,22 @@ export default function PostPage() {
     fetchPost();
     
   }, [postslug]);
+
+  useEffect(()=>{
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if(res.ok){
+          setRecentPosts(data.posts);
+        }
+      }
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message)
+    }
+  },[])
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -69,6 +87,20 @@ export default function PostPage() {
         <CallToAction/ >
       </div>
       <CommentSection postId={post._id}/>
+
+      <div className="flex flex-col mb-5 justify-center items-center">
+        <h1 className="text-xl mt-5">Recent articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts ? (
+            recentPosts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))
+          )
+          :(
+            'No recent posts yet'
+          )}
+        </div>
+      </div>
     </main>
     
   );
