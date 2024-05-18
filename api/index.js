e40a3e36282217592fbb649d1ea220525a978d05
shaminakarsha <shaminakarsha@gmail.com>
2024-05-18 19:08:13 +0530

    resolve the join error

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use("/api/comment", commentRoutes);
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path, join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // Creatgin a middleware to handle errors
